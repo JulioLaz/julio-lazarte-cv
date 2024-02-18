1485137c3b9b5c34260cf04c47c09ffa11278d3e
JulioLaz <julioalazarte@gmail.com>
2024-02-17 22:26:37 -0300

    link de presentacion

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,7 +8,8 @@ import { DatajsonService } from '../shared/datajson.service';
 })
 export class AboutComponent implements OnInit {
   // CARGA CV
-  url_cv:string='https://drive.google.com/file/d/1KaI5lxsa4CUAiamM5iG-I7THVByotUQV/view?usp=sharing' //CARGAR URL DEL CV
+  url_cv:string='https://docs.google.com/document/d/1WT_aBM08pkp3aNYfPmTjxrcojOnu_qFk/edit?usp=sharing&ouid=102564936615803527081&rtpof=true&sd=true' //CARGAR URL DEL CV
+  // url_cv:string='https://drive.google.com/file/d/1KaI5lxsa4CUAiamM5iG-I7THVByotUQV/view?usp=sharing' //CARGAR URL DEL CV
   // url_cv:string='https://drive.google.com/file/d/1ZMaWm0VuIqoqiS6ZT92wfcp8F3SD7Vy4/view?usp=sharing' //CARGAR URL DEL CV
   about: any;
   subtitles = ['Data Scientist Jr.', 'Full Stack Developer Jr.', 'Philosophy teacher'];
